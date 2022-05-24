b2e0aef8d1ef026bf6ff255ec7ddaa37a4da7e93
unknown <lukes90@seznam.cz>
2022-05-24 23:00:30 +0200

    comit unity

---
+++ Assets/Pohyb.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pohyb : MonoBehaviour
+{
+    private float SPEED = 1;
+    public Camera mojeCamera;
+    // Start is called before the first frame update
+    void Start()
+    {
+    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("right"))
+        {
+            //transform.position += Time.delta * Vector3.rigth;
+            transform.Rotate(0, 90f, 0);
+        }
+        if (Input.GetKeyDown("left"))
+        {
+            // transform.position += Time.delta * Vector3.left;
+            transform.Rotate(0, -90f, 0);
+        }
+        if (Input.GetKeyDown("up"))
+        {
+            //transform.position += Time.delta * Vector3.foward;
+            transform.position += SPEED * gameObject.transform.forward;
+        }
+        if (Input.GetKeyDown("down"))
+        {
+            transform.position -= SPEED * gameObject.transform.forward;
+            //transform.position += Time.delta * Vector3.back;
+        }
+
+        mojeCamera.transform.position = transform.position;
+        mojeCamera.transform.rotation = transform.rotation;
+    }
+}
+
---
+++ Assets/Rovina.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rovina : MonoBehaviour
+{
+
+    public GameObject mujPrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int x = -5; x < 5; x++)
+        {
+            for (int z = -5; z < 5; z++)
+            {
+                if ((x % 2 == 0 )&&(z % 2 == 0)) { Instantiate(mujPrefab, new Vector3(x, 1.5f, z), Quaternion.identity); }
+                
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
